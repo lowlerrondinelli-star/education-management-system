a95f680e9a57b656981b99afdfa53ed97f55bf4b
lowlerrondinelli-star <lowlerrondinelli@gmail.com>
2026-08-25 19:30:44 +0800

    chore: 启动时生成 Excel 预览数据

--- 启动本地原型.bat
+++ 启动本地原型.bat
@@ -2,6 +2,9 @@
 chcp 65001 >nul
 cd /d "%~dp0app"
 echo 正在启动教务管理系统本地原型...
+cd /d "%~dp0"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\extract-template-data.ps1"
+cd /d "%~dp0app"
 echo 打开地址: http://localhost:5177
 start "" "http://localhost:5177"
 python -m http.server 5177
